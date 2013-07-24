f47b2cfcc9793e973a5c68f6853b7591ac35817a
zhaoxiaohui <fire.guy@163.com>
2013-07-24 17:28:43 +0800

    test

--- googleImages.php
+++ googleImages.php
@@ -11,6 +11,6 @@ call get_images method by providing 3 parameters
 2.) cols - number of images per row
 3.) rows - number of rows
 *****************************/
-$gimage->get_images("cars", 4, 5);
+$gimage->get_images("周杰伦", 4, 5);
 
 ?>
